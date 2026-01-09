b7ce5ed8dc0ad1257b544e53c5a7eb461e8b5ddb
rmconsole-readonly-wk <gh_rosie-staging+readonly@workiva.com>
2026-01-09 19:33:48 +0000

    semver_audit_2.4.124

--- indexers/dart/lib/src/commands/version.dart
+++ indexers/dart/lib/src/commands/version.dart
@@ -1,4 +1,4 @@
 // This should be kept in sync with the version for semver-audit-dart that is
 // stored within pubspec.yaml
 
-const semverAuditDartVersion = '2.4.123';
+const semverAuditDartVersion = '2.4.124';
